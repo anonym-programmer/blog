e78a8bfd1eb6d8f3d0d48c7cd240572370c00579
RobertKrzywina <robert.krzywina@gmail.com>
2019-07-07 14:30:47 +0200

    Add server config

--- src/test/java/pl/robert/blog/BlogApplicationTests.java
+++ src/test/java/pl/robert/blog/BlogApplicationTests.java
@@ -1,13 +1,16 @@
 package pl.robert.blog;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class BlogApplicationTests {
+public class BlogApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
-
 }
