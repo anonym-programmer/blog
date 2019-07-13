a3f66182ff7e4309d9625404a1703ed510f242b3
RobertKrzywina <robert.krzywina@gmail.com>
2019-07-13 15:42:04 +0200

    Add login form on front

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ client/src/app/login/login.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
+import { LoginDto } from '../shared/LoginDto.model';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+  
+  formGroup: FormGroup;
+  dto = new LoginDto();
+
+  constructor(private router: Router, private http: HttpClient, private formBuilder: FormBuilder) { 
+    this.createForm();
+  }
+
+  ngOnInit() {
+    sessionStorage.setItem('token', '');
+  }
+
+  createForm() {
+    this.formGroup = this.formBuilder.group({
+      username: [''],
+      password: ['']
+    });
+  }
+
+  login(username: String, password: String)  {
+    let url = 'http://localhost:8080/login';
+
+    this.dto.username = username;
+    this.dto.password = password;
+
+    this.http.post(url, this.dto).subscribe(isValid => {
+      if (isValid) {
+        sessionStorage.setItem(
+          'token',
+          btoa(username + ':' + password)
+        );
+        this.router.navigate(['']);
+      } else {
+        alert("Authentication failed.");
+      }
+    });
+  }
+}
