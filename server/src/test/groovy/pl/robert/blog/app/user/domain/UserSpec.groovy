6b251146a0b305bc20a581c0856b5c601bf3b0f0
RobertKrzywina <robert.krzywina@gmail.com>
2019-07-30 22:37:21 +0200

    Optimize tests

--- server/src/test/groovy/pl/robert/blog/app/user/domain/UserSpec.groovy
+++ server/src/test/groovy/pl/robert/blog/app/user/domain/UserSpec.groovy
@@ -1,7 +1,5 @@
 package pl.robert.blog.app.user.domain
 
-import pl.robert.blog.app.user.domain.dto.ChangePasswordDto
-import pl.robert.blog.app.user.domain.exception.InvalidPasswordException
 import spock.lang.Shared
 import spock.lang.Specification
 
@@ -11,34 +9,47 @@ import lombok.experimental.FieldDefaults
 import java.util.concurrent.ConcurrentHashMap
 
 import pl.robert.blog.app.user.domain.dto.UserDetailsDto
+import pl.robert.blog.app.user.domain.dto.ChangePasswordDto
+import pl.robert.blog.app.user.domain.exception.InvalidPasswordException
 
 import org.springframework.security.core.userdetails.UsernameNotFoundException
 
-@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@FieldDefaults(level = AccessLevel.PRIVATE)
 class UserSpec extends Specification {
 
     @Shared
     UserFacade facade
 
     @Shared
+    User user
+
+    @Shared
     ConcurrentHashMap<Long, User> db = new ConcurrentHashMap<>()
 
     def setupSpec() {
         facade = new UserConfiguration().facade(db)
+        user = new User(1L, 'Robert', '!23QweAsd#@!', 'ADMIN', new UserDetails())
     }
 
-    def 'Should find one user with default fields'() {
-        when: 'we add user to db'
-        db.put(1L, new User(1L, 'Robert', '!23QweAsd#@!', 'ADMIN', new UserDetails()))
+    def cleanup() {
+        if (!db.isEmpty()) {
+            db.clear()
+        }
+    }
 
-        and: 'we find user'
+    def setup() {
+        if (db.isEmpty()) {
+            db.put(1L, user)
+        }
+    }
+
+
+    def 'Should find one user with default fields'() {
+        when: 'we find user'
         def user = facade.find()
 
         then: 'user contains default fields'
         checkDefaultFields(user)
-
-        and: 'we delete user from db'
-        db.clear()
     }
 
     def checkDefaultFields(user) {
@@ -46,7 +57,10 @@ class UserSpec extends Specification {
     }
 
     def 'Should throw an exception cause user not found'() {
-        when: 'we try to find user'
+        when: 'we delete user from db'
+        db.clear()
+
+        and: 'we try to find user'
         facade.find()
 
         then: 'exception is thrown'
@@ -54,10 +68,7 @@ class UserSpec extends Specification {
     }
 
     def 'Should save and update details'() {
-        when: 'we add user to db'
-        db.put(1L, new User(1L, null, null, null, new UserDetails()))
-
-        and: 'we save details'
+        when: 'we save details'
         facade.saveDetails(new UserDetailsDto('<h1>Hello</h1>'))
 
         then: 'user has this details'
@@ -68,9 +79,6 @@ class UserSpec extends Specification {
 
         then: 'user details has been updated'
         facade.findDetails().details == '<h1>HelloWorld</h1>'
-
-        and: 'we delete user from db'
-        db.clear()
     }
 
     def 'Should change password'() {
@@ -78,17 +86,14 @@ class UserSpec extends Specification {
         String oldPassword = 'pass123'
         String newPassword = 'pass321'
 
-        when: 'we add user to db'
-        db.put(1L, new User(1L, 's', oldPassword, 's', new UserDetails()))
+        when: 'we set user password'
+        db.get(1L).password = oldPassword
 
         and: 'we change password'
         facade.changePassword(new ChangePasswordDto(oldPassword, newPassword))
 
         then: 'user has this new password'
         facade.find().password == newPassword
-
-        and: 'we delete user from db'
-        db.clear()
     }
 
     def 'Should throw an exception cause given password not equal actual password'() {
@@ -97,12 +102,12 @@ class UserSpec extends Specification {
         String actualPassword = 'pass321'
 
         when: 'we add user to db'
-        db.put(1L, new User(1L, 's', actualPassword, 's', new UserDetails()))
+        db.get(1L).password = actualPassword
 
         and: 'we try to change password'
         facade.changePassword(new ChangePasswordDto(password, 'newPass'))
 
         then: 'exception is thrown'
-        thrown(InvalidPasswordException)
+        thrown InvalidPasswordException
     }
 }
