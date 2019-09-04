c02a0d4b67babda1ca39944224109dae7858fc1e
RobertKrzywina <robert.krzywina@gmail.com>
2019-09-04 21:03:37 +0200

    Add test about adding new post

---
+++ server/src/test/pl/robert/blog/app/post/domain/PostSpec.groovy
@@ -0,0 +1,14 @@
+package pl.robert.blog.app.post.domain
+
+import spock.lang.Specification
+
+class PostSpec extends Specification {
+
+
+    def 'Should add post'() {
+        when: 'we add post'
+
+        then: 'system has this post'
+
+    }
+}
